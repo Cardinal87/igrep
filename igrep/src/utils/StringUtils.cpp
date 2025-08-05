4700b172c7763b56df7d4b17c90e6c01d7998512
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-08-05 21:23:03 +0300

    feat(utils): add implementation of StringUtils

---
+++ igrep/src/utils/StringUtils.cpp
@@ -0,0 +1,38 @@
+#include "../../include/utils/StringUtils.h"
+#include<unordered_set>
+
+
+using namespace std;
+
+namespace include::utils{
+
+    string StringUtils::normalize_line(string line){
+        static const unordered_set<char> punctuation = { ',', '.', '\'', '\"', '\\', ':', '/', ';','[','{', '}', ']', '@', '#', '$', '!', '?', '-', '+', '=', '&', '^', '%', '~', '_'};
+
+		for (auto it = line.begin(); it != line.end(); it++) {
+			if (punctuation.count(*it) == 1) {
+				*it = ' ';
+			}
+		}
+
+		return remove_extra_spaces(line);
+    }
+
+
+    
+	string StringUtils::remove_extra_spaces(const string& line ){
+        string clean_line = "";
+		bool is_prev_space = false;
+		for (auto it = line.begin(); it != line.end(); it++) {
+			if (*it == ' ' && is_prev_space) {
+				continue;
+			}
+
+			clean_line += *it;
+			is_prev_space = *it == ' ';
+		}
+
+		return clean_line;
+    }	
+	
+}
