bafceab8f634a3fa354eb971e83515fde1d95b5a
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-08-27 16:49:17 +0300

    feat(utils): update StringUtils

--- igrep/include/utils/StringUtils.h
+++ igrep/include/utils/StringUtils.h
@@ -7,7 +7,7 @@ namespace igrep::utils{
 
         public:
             static std::string normalize_line(std::string line);
-
+            static std::string to_lower_case_copy(const std::string& line);
 
         private:
             static std::string remove_extra_spaces(const std::string& line);
--- igrep/src/utils/StringUtils.cpp
+++ igrep/src/utils/StringUtils.cpp
@@ -6,19 +6,28 @@ using namespace std;
 
 namespace igrep::utils{
 
-    string StringUtils::normalize_line(string line){
-        static const unordered_set<char> punctuation = { ',', '.', '\'', '\"', '\\', ':', '/', ';','[','{', '}', ']', '@', '#', '$', '!', '?', '-', '+', '=', '&', '^', '%', '~', '_'};
+    string StringUtils::normalize_line(string line) {
+        static const unordered_set<char> punctuation = { ',', '.', '\'', '\"', '\\', ':', '/', ';','[','{', '}', ']', '(',')' , '@', '#', '$', '!', '?', '-', '+', '=', '&', '^', '%', '~', '_'};
 
 		for (auto it = line.begin(); it != line.end(); it++) {
 			if (punctuation.count(*it) == 1) {
 				*it = ' ';
+			}
+			else{
+				*it = static_cast<char>(tolower(static_cast<unsigned char>(*it)));
 			}
 		}
 
 		return remove_extra_spaces(line);
     }
 
-
+	string StringUtils::to_lower_case_copy(const string& line){
+		string lower_copy;
+		for(auto& ch : line){
+			lower_copy += static_cast<char>(tolower(static_cast<unsigned char>(ch)));
+		}
+		return lower_copy;
+	}
     
 	string StringUtils::remove_extra_spaces(const string& line ){
         string clean_line = "";
