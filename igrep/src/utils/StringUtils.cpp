6de87c543a5bf4ea6038da9cdc8b51b122fe785b
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-08-28 20:39:34 +0300

    fix(utils): add removing first and last space in string

--- igrep/src/utils/StringUtils.cpp
+++ igrep/src/utils/StringUtils.cpp
@@ -32,13 +32,19 @@ namespace igrep::utils{
 	string StringUtils::remove_extra_spaces(const string& line ){
         string clean_line = "";
 		bool is_prev_space = false;
+		bool find_first_char = false;
 		for (auto it = line.begin(); it != line.end(); it++) {
-			if (*it == ' ' && is_prev_space) {
+			if (*it == ' ' && (is_prev_space || !find_first_char)) {
 				continue;
 			}
-
+			if (!find_first_char){
+				find_first_char = true;
+			}
 			clean_line += *it;
 			is_prev_space = *it == ' ';
+		}
+		if (is_prev_space){
+			clean_line.pop_back();
 		}
 
 		return clean_line;
