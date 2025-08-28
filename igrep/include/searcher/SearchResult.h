536b7880310d48dff788ab82537d47f5e4e718cf
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-08-28 21:09:42 +0300

    feat(searcher): define equals operator for SearchResult

--- igrep/include/searcher/SearchResult.h
+++ igrep/include/searcher/SearchResult.h
@@ -7,10 +7,18 @@ namespace igrep::searcher
 {
    struct SearchResult
    {
-        const std::string result;
-        const std::string file_path;
-        const int line;
+      const std::string result;
+      const std::string file_path;
+      const int line;
+
+
+      bool operator==(const SearchResult& other) const{
+         return result == other.result &&
+                file_path == other.file_path &&
+                line == other.line;
+      }
    };
     
     
+   
 }
